4e8988b5593525ffc3d744427077bdbf7759a10f
jiri.dudek <jiri.dudek@unicorn.com>
2018-03-21 21:32:07 +0100

    Add performance to global for uu5 tests.

--- test/withUU5/tools/setup.js
+++ test/withUU5/tools/setup.js
@@ -3,6 +3,7 @@ const Window = require('window');
 global.window = new Window();
 global.document = window.document;
 global.navigator = window.navigator;
+global.performance = window.performance;
 
 export default function () {
 
